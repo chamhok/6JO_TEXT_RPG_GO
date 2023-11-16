d507a106b475dfec1d58c212d2c6945544a234f0
LaBear2 <op9339291@gmail.com>
2023-11-16 15:15:26 +0900

    test jaeyeol

--- 6Jo_Text_Game/Program.cs
+++ 6Jo_Text_Game/Program.cs
@@ -210,7 +210,7 @@ class Program
                 GameData.I.GetCharacters().Select(x => x.ToString()).ToList().ForEach(Console.WriteLine);
         Console.WriteLine("TT"      );
         Console.WriteLine();
-        Console.WriteLine("어렵다 어려워");
+        Console.WriteLine("어렵징? 어려워");
         
     }
 }
