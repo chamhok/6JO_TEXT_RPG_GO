96954db9d2968859b2b611f29d2843b604d4f16c
강건욱 <133373768+chamhok@users.noreply.github.com>
2023-11-17 21:13:09 +0900

    스토리 시작 부분 캐릭터 생성 직업 선택

--- 6Jo_Text_Game/Program.cs
+++ 6Jo_Text_Game/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< Updated upstream
 using System.Text;
-=======
 using System.Linq;
->>>>>>> Stashed changes
+
 
 
 // 게임 데이터를 저장하는 싱글톤 클래스 정의 
@@ -219,9 +217,13 @@ class Program
 }
 public class StartScreen
 {
+        public string DirectoryGetParent(string directoryInfo)
+        {
+                return Directory.GetParent(Directory.GetParent(Directory.GetParent(directoryInfo).ToString()).ToString()).ToString();
+        }
         private void StartScreenText()
         {
-                string currentDirectory = Directory.GetCurrentDirectory();
+                string currentDirectory = DirectoryGetParent(Directory.GetCurrentDirectory());
                 // Specify the file name (change it to your actual file name)
                 string fileName = "StartScreenText.txt";
 
@@ -237,7 +239,7 @@ public class StartScreen
                                 foreach (char cha in line)
                                 {
                                         Console.Write(cha);
-                                        /*Thread.Sleep(50);*/
+                                        if (!Console.KeyAvailable) Thread.Sleep(50);
                                 }
                                 Console.WriteLine();
                         }
@@ -248,27 +250,108 @@ public class StartScreen
                 {
                         Console.WriteLine($"Error reading the file: {ex.Message}");
                 }
+        }
 
-                Console.Write("당신의 이름을 입력하십시오. : ");
+        public void CharName(out string charName)
+        {
+                foreach (var item in "\n당신의 이름을 입력하십시오. ")
+                {
+                        Console.Write(item);
+                        Thread.Sleep(50);
+                };
+                while (true)
+                {
+                        Console.Write(">> ");
+                        charName = Console.ReadLine();
+                        if (!(charName == ""))
+                        {
+                                break;
+                        }
+                }
+
+        }
+        public void CharJob(out Job? job)
+        {
+                Console.Clear();
+                string weapon =
+                          $"1. 올곧은 빛의 검 루미나소드\r\n" +
+                          $"검은 마치 어둠을 가를 것처럼 보입니다.\r\n" +
+                          $"투명한 검날에서 빛나는 힘은 마치 마법의 속삭임처럼 들리며,\r\n" +
+                          $"일정한 폭으로 검은 선들이 새겨져 있는 문양은 신비롭습니다.\r\n" +
+                          $"이것으로 나아가면 세계의 비밀스러운 모험을 만날 것입니다.\r\n" +
+                          $"검의 끝에는 지혜와 결단이 서려 있으며,\r\n" +
+                          $"모든 어려움을 극복할 수 있는 힘이 내재되어 있습니다.\r\n" +
+                          $"이 무기를 휘두르며, 나만의 운명을 쓰러뜨리고새로운 세계로 나아갈 것입니다. \n" +
+                          $"\n" +
+                          $"2. 노래하는 환상의 완드 리코드로스\r\n" +
+                          $"오래된 마법 지팡이, \r\n" +
+                          $"그 반짝이는 지팡이는 마법의 힘을 담고 있습니다. \r\n" +
+                          $"누군가 그 지팡이를 휘두르면, \r\n" +
+                          $"공중에 특별한 음향의 고요한 빛이 퍼져나가 마법의 소리로 변합니다. \r\n" +
+                          $"이 마법 지팡이는 마법의 세계에서 흘러나오는 고대의 지식을 기록하고, \r\n" +
+                          $"그 속에서 다양한 이야기와 모험을 펼칠 수 있는 유용한 도구입니다.\n" +
+                          $"" +
+                          $"3.  자비없는 필멸의 활 " +
+                          $"4. 그저 그는 도덕책";
+                foreach (var item in weapon)
+                {
+                        Console.Write(item);
+                        Thread.Sleep(50);
+                }
+                Console.Write("\n당신은 어떠한 무기를 선택하실 겁니까? : \n>> ");
+                int input = CheckValidInput(1, 4);
+
+                switch (input)
+                {
+                        case 1:
+                                job = Job.가디언;
+                                break;
+                        case 2:
+                                job = Job.위자드;
+                                break;
+                        case 3:
+                                job = Job.레인저;
+                                break;
+                        case 4:
+                                job = Job.성직자;
+                                break;
+                        default:
+                                job = null;
+                                break;
+                }
 
         }
         public StartScreen()
         {
+                string charName;
+                Job? charJob;
                 StartScreenText();
-                string CharName = Console.ReadLine();
-                Character character = new Character(CharName, 1, 10, 10, 10, 100, 500, Job.가디언, 10, 10, Attribute.풍);
+
+                CharName(out charName);
+                CharJob(out charJob);
+
+
+                Character character = new Character(charName, 1, 10, 10, 10, 100, 500, Job.가디언, 10, 10, Attribute.풍);
                 character.Add();
                 Console.WriteLine($"\n당신의 이름은 {character.Name}입니다.");
-
-                // 캐릭터 생성 및 게임 데이터에 추가 후 목록 출력
-                // character.Add(new Character("ㅇㅇ", 100, 100, 100, 100, 100, 200, Job.가디언, 10, 10, Attribute.풍));
-                //Monster monster = new Monster("Test", 1, 1, 1, 1, 1, 1, Species.고블린,1,1, Attribute.풍);
-                //monster.Add(new Monster("Test", 1, 1, 1, 1, 1, 1, Species.고블린, 1, 1, Attribute.풍));
-                //Monster monsterb = new Monster("Test2", 1000, 1000, 1000, 1000, 1000, 1000, Species.고블린,1,1, Attribute.풍);
-                //monster.Add(new Monster("Test2", 1000, 1000, 1000, 1000, 1000, 1000, Species.고블린, 1, 1, Attribute.풍));
+                Console.WriteLine($"\n당신의 직업은 {character.Job}입니다.");
 
 
+        }
+        static int CheckValidInput(int min, int max)
+        {
+                while (true)
+                {
+                        string input = Console.ReadLine() ?? " ";
 
+                        bool parseSuccess = int.TryParse(input, out var ret);
+                        if (parseSuccess)
+                        {
+                                if (ret >= min && ret <= max)
+                                        return ret;
+                        }
 
+                        Console.WriteLine("잘못된 입력입니다.");
+                }
         }
 }
